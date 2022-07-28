59c84f07c2ba8e5a49b2b889b18fdf1a00cb87a3
hashvizard <hashvizard@gmail.com>
2022-07-28 12:03:59 +0530

    Refactor

--- credentials.js
+++ credentials.js
@@ -1,13 +1,13 @@
 module.exports = {
-  // BACKEND_URL:'#########################',
-  BACKEND_URL:'http://10.0.2.2:8000/api/',
-  GOOGLE_WEB_CLIENT_ID: '#########################',
-  GOOGLE_MAP_API: '#########################',
-  CODE_PUSH: '#########################',
-  //AWS_S3
-  KEY_PREFIX: '#########################', 
-  BUCKET: '#########################',
-  REGION: '#########################',
-  ACCESS_KEY: '#########################',
-  SECRET_KEY: '*******************',
-};
+    BACKEND_URL:'https://tooday.in/api/',
+    // BACKEND_URL:'http://10.0.2.2:8000/api/',
+    GOOGLE_WEB_CLIENT_ID: '128578158131-ghr9c1i0k1q1084kaptun7norts3gkud.apps.googleusercontent.com',
+    GOOGLE_MAP_API: 'AIzaSyBtw8RmNNc0aGXUBW5L1dYnWbgDCxdfnFg',
+    CODE_PUSH: 'appcenter codepush release-react -a hashvizard-gmail.com/TooDay -d Staging',
+    //AWS_S3
+    KEY_PREFIX: 'videos', 
+    BUCKET: 'tooday',
+    REGION: 'ap-south-1',
+    ACCESS_KEY: 'AKIAVIYWH7VTYHRLDVON',
+    SECRET_KEY: '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',
+  };
