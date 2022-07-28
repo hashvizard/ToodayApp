c94169d6ee54af920222d1c9c6d3a8f6e9d482a9
hashvizard <hashvizard@gmail.com>
2022-07-28 11:44:24 +0530

    Credentials

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Route from './src/navigation/main'
-import { QueryClient, QueryClientProvider } from 'react-query'
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import withCodePush from './CodePush'
@@ -9,11 +8,6 @@ import { navigationRef } from './RootNavigation';
 import { NavigationContainer } from '@react-navigation/native';
 import { connect } from 'react-redux';
 import { updateCheck } from './src/redux/actions';
-
-
-const queryClient = new QueryClient({
-  defaultOptions: { queries: { refetchInterval: false, staleTime: Infinity } }
-})
 
 const theme = {
   ...DefaultTheme,
@@ -31,11 +25,9 @@ const App = () => {
     <SafeAreaProvider>
       <StatusBar barStyle='dark-content' />
       <PaperProvider theme={theme}>
-          <QueryClientProvider client={queryClient}>
             <NavigationContainer ref={navigationRef}>
               <Route />
             </NavigationContainer>
-          </QueryClientProvider>
       </PaperProvider>
     </SafeAreaProvider>
   )
